a4660cdf8647299e1041f343e771844704a2f56e
innateso <inna.tesoro@gmail.com>
2022-03-14 10:12:52 +1300

    first commit

---
+++ connection.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+// start editable
+
+// as defined in 'Database Access' on Atlas/Mongo
+
+const dbname = "Formativetwo";
+const username = "yoobeeteamone";
+const password = "yoobeeteamone";
+
+// with backticks
+const url = `mongodb+srv://${username}:${password}@formativetwo.kyx6g.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+// end editable
+
+// connection start
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+
+  useUnifiedTopology: true,
+});
+
+// feedback to let us know we succeeded
+
+mongoose.connection.on("connected", (err, res) => {
+  console.log("Success! Connected to MongoDB");
+});
+
+// let us know we failed
+
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to MongoDB ", err);
+});
+
+// end connection code
